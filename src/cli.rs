5153c4c0ff80a2829d1ecf0fd103b303658c3dda
Thomas Gassmann <thomas@gassmann.dev>
2024-08-19 15:15:11 +0200

    support multiple subdomains/iptypes at once
    my rust skills are still limited, please don't judge
    
    Signed-off-by: Thomas Gassmann <thomas@gassmann.dev>

--- src/cli.rs
+++ src/cli.rs
@@ -7,26 +7,31 @@ use structopt::StructOpt;
 
 use crate::vercel::{add_dns_record, Record};
 
-fn get_public_ip(ip_type: &IpType) -> Result<String> {
-    let resolver = match *ip_type {
-        IpType::IPV4 => vec![
-            BoxToResolver::new(dns::OPENDNS_RESOLVER_V4),
-            BoxToResolver::new(dns::GOOGLE_DNS_TXT_RESOLVER_V4),
-        ],
-        IpType::IPV6 => vec![
-            BoxToResolver::new(dns::OPENDNS_RESOLVER_V6),
-            BoxToResolver::new(dns::GOOGLE_DNS_TXT_RESOLVER_V6),
-        ],
-    };
+fn get_public_ips(ip_types: &Vec<IpType>) -> Result<Vec<(IpType, String)>> {
+    let mut res: Vec<(IpType, String)> = Vec::new();
+    for ip_type in ip_types.iter() {
+        let resolver = match *ip_type {
+            IpType::IPV4 => vec![
+                BoxToResolver::new(dns::OPENDNS_RESOLVER_V4),
+                BoxToResolver::new(dns::GOOGLE_DNS_TXT_RESOLVER_V4),
+            ],
+            IpType::IPV6 => vec![
+                BoxToResolver::new(dns::OPENDNS_RESOLVER_V6),
+                BoxToResolver::new(dns::GOOGLE_DNS_TXT_RESOLVER_V6),
+            ],
+        };
 
-    match executor::block_on(public_ip::resolve_address(resolver.to_resolver())) {
-        Some(ip) => Ok(ip.to_string()),
-        None => Err(eyre!("Unable to get public IP.")),
+        match executor::block_on(public_ip::resolve_address(resolver.to_resolver())) {
+            Some(ip) => res.push((ip_type.clone(), ip.to_string())),
+            None => return Err(eyre!("Unable to get public IP.")),
+        }
     }
+
+    return Ok(res);
 }
 
 arg_enum! {
-    #[derive(Debug,PartialEq)]
+    #[derive(Debug,PartialEq,Clone)]
     pub enum IpType {
         IPV4,
         IPV6,
@@ -44,10 +49,10 @@ pub struct Args {
     pub domain: String,
 
     #[structopt(short, long, env = "VDDNS_SUBDOMAIN")]
-    pub subdomain: String,
+    pub subdomains: Vec<String>,
 
     #[structopt(short, long, possible_values = &IpType::variants(), case_insensitive = true, default_value = "ipv4", env = "VDDNS_IP_TYPE")]
-    pub ip_type: IpType,
+    pub ip_types: Vec<IpType>,
 
     #[structopt(long, default_value = "3600", env = "VDDNS_TTL")]
     pub ttl: i64,
@@ -57,7 +62,7 @@ pub struct Args {
 }
 
 pub fn run(args: Args) -> Result<()> {
-    let ip = match get_public_ip(&args.ip_type) {
+    let ips = match get_public_ips(&args.ip_types) {
         Ok(ip) => ip,
         Err(e) => {
             error!("Unable to get public ip. {}", e.to_string());
@@ -65,24 +70,29 @@ pub fn run(args: Args) -> Result<()> {
         }
     };
 
-    let rec = Record::new(
-        args.subdomain,
-        ip,
-        match args.ip_type {
-            IpType::IPV4 => String::from("A"),
-            IpType::IPV6 => String::from("AAAA"),
-        },
-        args.ttl,
-    );
-
-    match add_dns_record(&args.domain, &args.token, rec) {
-        Ok(_) => {
-            info!("Record added / updated sucessfully");
-            return Ok(());
-        }
-        Err(e) => {
-            error!("Unable to add / update the record. {}", e.to_string());
-            return Ok(());
+    for subdomain in args.subdomains.iter() {
+        for (ip_type, ip) in ips.iter() {
+            let rec = Record::new(
+                subdomain.to_string(),
+                ip.to_string(),
+                match *ip_type {
+                    IpType::IPV4 => String::from("A"),
+                    IpType::IPV6 => String::from("AAAA"),
+                },
+                args.ttl,
+            );
+        
+            match add_dns_record(&args.domain, &args.token, rec) {
+                Ok(_) => {
+                    info!("Record added / updated sucessfully");
+                }
+                Err(e) => {
+                    error!("Unable to add / update the record. {}", e.to_string());
+                    return Ok(());
+                }
+            }
         }
     }
+
+    return Ok(());
 }
